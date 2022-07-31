49ce5d035ca4fbcc42663eff0d7291b67e10a5eb
Adarsh Shukla <shuklaadarsh1932@gmail.com>
2022-07-31 10:39:53 +0530

    Initial Commit

---
+++ LearnMapKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnMapKit
+//
+//  Created by Adarsh Shukla on 31/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnMapKit/LearnMapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnMapKitApp.swift
+//  LearnMapKit
+//
+//  Created by Adarsh Shukla on 31/07/22.
+//
+
+import SwiftUI
+
+@main
+struct LearnMapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
